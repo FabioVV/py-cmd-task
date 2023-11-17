133c13d741b034f1f5a7cbd67c3622b41cbbdb57
FabioVV <fabio-varela@protonmail.com>
2023-11-17 15:13:45 -0300

    started main structure

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from prettytable import PrettyTable
 from todo import todo
+import subprocess
+import platform
 import json
 import os
 
+# Initialize main variables - (I may not be using them right now.)
+commands = ['']
+
 
 def main():
-  table = PrettyTable()
 
+  match platform.system():
+    case 'Windows':
+      subprocess.call('cls', shell=True)
+    case _:
+      subprocess.call('clear', shell=True)
+
+
+  subprocess.call('echo Hello, friend.', shell=True)
+
+
+
+
+
+  table = PrettyTable()
+  
 
 
 
---
+++ todo/todo.py
@@ -0,0 +1,25 @@
+import json
+
+# Initialize main variables - (I may not be using them right now.)
+item = {}
+
+def add():
+    pass
+
+def complete():
+    pass
+
+def delete():
+    pass
+
+def load():
+    pass
+
+def store():
+    pass
+
+def countPending():
+    pass
+
+def printTodos(todos):
+    pass
